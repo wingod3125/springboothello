fe4628f656422e2e3e31a54b384b423bfea6a7c4
wingod3125 <wgxhn@qq.com>
2017-07-15 02:54:11 +0800

    first commit

---
+++ src/main/java/springboot/example/Application.java
@@ -0,0 +1,22 @@
+package springboot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by WG on 2017/7/15.
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+    @RequestMapping("/")
+    String home(){
+        return "hello";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ src/main/java/springboot/example/dao/Deparment.java
@@ -0,0 +1,7 @@
+package springboot.example.dao;
+
+/**
+ * Created by WG on 2017/7/15.
+ */
+public class Deparment {
+}
